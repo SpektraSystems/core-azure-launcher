5a1a5201eea14508b0613b81098303bbb4e671d2
Amit Malik <contact2amitmalik@gmail.com>
2018-08-29 14:15:06 -0700

    Update bootstrap-dev.sh

--- bootstrap-dev.sh
+++ bootstrap-dev.sh
@@ -23,6 +23,8 @@ function deploy_ingress
 {
     #az login --service-principal -u $spnappid -p $spnapppassword --tenant $spntenantid
     #az aks get-credentials -g $rgname -n $k8name
+    echo $cfg | base64 --decode > config.txt
+    export KUBECONFIG=config.txt
     kubectl apply -f https://raw.githubusercontent.com/SpektraSystems/core-azure-launcher/master/manifest/ingress-mandatory.yaml
     kubectl apply -f https://raw.githubusercontent.com/SpektraSystems/core-azure-launcher/master/manifest/ingress-cloud-generic.yaml
  while [[ "$(kubectl get svc ingress-nginx -o jsonpath='{.status.loadBalancer.ingress[0].ip}')" = '' ]]; do sleep 3; done
